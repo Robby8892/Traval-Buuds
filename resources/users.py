38a4ec1463c1c870d5991cb85448d6c9b7ab128b
roberto cortes <robertcortesgrimm223@gmail.com>
2020-02-15 18:50:14 -0600

    registration works on user resource

--- resources/users.py
+++ resources/users.py
@@ -2,6 +2,10 @@ import models
 
 from flask import Blueprint, jsonify, request 
 
+from flask_bcrypt import generate_password_hash
+
+from playhouse.shortcuts import model_to_dict
+
 users = Blueprint('users', 'users')
 
 
@@ -16,6 +20,31 @@ def test_routes():
 def register_user():
 	payload = request.get_json()
 
-	print(payload)
+	payload['email'] = payload['email'].lower()
+	payload['username'] = payload['username'].lower()
 
-	return 'check terminal'	
+	try:
+		models.User.get(models.User.email == payload['email'] or models.User.username == payload['username'] )	
+
+		return jsonify(
+			data={},
+			message=f'A users with that email already exists',
+			status=401
+			), 401
+
+	except models.DoesNotExist:
+
+		created_user = models.User.create(
+			username=payload['username'],
+			email=payload['email'],
+			password=generate_password_hash(payload['password'])
+			)
+
+		user_dict = model_to_dict(created_user)
+		user_dict.pop('password')
+
+		return jsonify(
+			data=user_dict,
+			message='You have successfully registered {} to the site'.format(user_dict['username']),
+			status=201
+			), 201
