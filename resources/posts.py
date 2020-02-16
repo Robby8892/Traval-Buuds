bef0b2668d0655615c2ed9b7880cc5d4f4291480
roberto cortes <robertcortesgrimm223@gmail.com>
2020-02-16 11:59:54 -0600

    new post is created by logged in user

--- resources/posts.py
+++ resources/posts.py
@@ -1,10 +1,33 @@
 import models 
 
 from flask import Blueprint, jsonify, request
+from flask_login import current_user
+
+from playhouse.shortcuts import model_to_dict
 
 posts = Blueprint('posts', 'posts')
 
 
-@posts.route('/')
-def test_route():
-	return 'check terminal'
+@posts.route('/', methods=['POST'])
+def create_post():
+	payload = request.get_json()
+
+	print(payload)
+
+	post = models.Post.create(
+		title=payload['title'],
+		place=payload['place'],
+		photo=payload['photo'],
+		story=payload['story'],
+		user=current_user.id
+		)
+
+	post_dict = model_to_dict(post)
+
+	post_dict['user'].pop('password')
+
+	return jsonify(
+		data=post_dict,
+		message='You have successfully created a new post as {}'.format(post_dict['user']['username']),
+		status=201
+		), 201
