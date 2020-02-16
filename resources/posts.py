ee6a3677ac760baa42ab6e148271b3d4fb76eed7
roberto cortes <robertcortesgrimm223@gmail.com>
2020-02-16 13:41:29 -0600

    shows users posts, converted bytes to strings for json response

--- resources/posts.py
+++ resources/posts.py
@@ -1,18 +1,17 @@
 import models 
-
+import json
 from flask import Blueprint, jsonify, request
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 from playhouse.shortcuts import model_to_dict
 
 posts = Blueprint('posts', 'posts')
 
-
+# create route for posts
 @posts.route('/', methods=['POST'])
+@login_required
 def create_post():
 	payload = request.get_json()
-
-	print(payload)
 
 	post = models.Post.create(
 		title=payload['title'],
@@ -31,3 +30,25 @@ def create_post():
 		message='You have successfully created a new post as {}'.format(post_dict['user']['username']),
 		status=201
 		), 201
+
+
+@posts.route('/', methods=['GET'])
+def posts_index():
+
+	current_user_posts = [model_to_dict(post) for post in current_user.posts]
+
+	print(current_user_posts)
+
+	for post in current_user_posts:
+		post['photo'] = post['photo'].decode('utf8').replace("",'')
+		print('_' * 20)
+		post['user'].pop('password')
+
+
+
+
+	return jsonify(
+		data=current_user_posts,
+		message=f'You have retrived all posts by {current_user.email}, there is a total of {len(current_user_posts)}',
+		status=200
+		), 200
